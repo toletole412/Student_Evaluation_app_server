c532e28456c22a24daf2ba7f53568d56c8c99cd9
toletole412 <yoonjioh90@gmail.com>
2018-04-03 20:14:53 +0200

    add oneToMany relation with evaluation

--- src/teachers/entity.ts
+++ src/teachers/entity.ts
@@ -1,6 +1,7 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
 import { Exclude } from 'class-transformer'
 import { MinLength, IsString, IsEmail } from 'class-validator'
+import { Evaluation } from '../students/entities'
 import * as bcrypt from 'bcrypt'
 
 @Entity()
@@ -18,6 +19,9 @@ export default class Teacher extends BaseEntity {
   @Column('text')
   @Exclude({ toPlainOnly: true })
   password: string
+
+  @OneToMany(_ => Evaluation, evaluation => evaluation.teacher, {eager:true} )
+  evaluations: Evaluation[]
 
   async setPassword(rawPassword: string) {
     const hash = await bcrypt.hash(rawPassword, 10)
