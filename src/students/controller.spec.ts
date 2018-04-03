cd3b32c8c93e00ec6fe5395de3541b167fe89bf7
toletole412 <yoonjioh90@gmail.com>
2018-04-03 22:32:16 +0200

    Add test for RESTful api

--- src/students/controller.spec.ts
+++ src/students/controller.spec.ts
@@ -12,7 +12,7 @@ beforeAll(async () => {
 //   "endDate": '2018-04-19'
 // }
 //
-// describe('StudentController', () => {
+// describe('StudentController_check if post new Batch or not', () => {
 //   test('/batches', async () => {
 //
 //     await request(await app.callback())
@@ -22,3 +22,64 @@ beforeAll(async () => {
 //     .expect(201)
 //   })
 // })
+
+describe('StudentController_check if fetch all batches or not', () => {
+  test('/fetchBatches', async () => {
+
+    await request(await app.callback())
+    .get('/fetchBatches')
+    .set('Accept', 'application/json')
+    .expect(200)
+  })
+})
+
+// const addStudent = {
+//   "fullName": "Janet Jeon",
+//   "picture": "https://imagizer.imageshack.com/v2/280x200q90/922/mWnlLK.png"
+// }
+
+// describe('StudentController_check if add student well or not', () => {
+//   test('/students', async () => {
+//
+//     await request(await app.callback())
+//     .post('/students')
+//     .set('Accept', 'application/json')
+//     .send(addStudent)
+//     .expect(201)
+//   })
+// })
+
+
+describe('StudentController_check if get specific student well or not', () => {
+  test('/students/1', async () => {
+
+    await request(await app.callback())
+    .get('/students/1')
+    .set('Accept', 'application/json')
+    .expect(200)
+  })
+})
+
+const edit = {
+  fullName: "Yoonji Oh"
+}
+
+describe('StudentController_check if edit student well or not', () => {
+  test('/students/1', async () => {
+
+    await request(await app.callback())
+    .put('/students/1')
+    .set('Accept', 'application/json')
+    .send(edit)
+    .expect(200)
+  })
+})
+
+describe('StudentController_check if delete student well or not', () => {
+  test('/students/5', async () => {
+
+    await request(await app.callback())
+    .delete('/students/5')
+    .expect(200)
+  })
+})
