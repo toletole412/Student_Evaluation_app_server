836a1b1d43e60405883d0dafb589cf60a3b29364
toletole412 <yoonjioh90@gmail.com>
2018-04-03 20:17:02 +0200

    create signup for test

--- src/teachers/controller.ts
+++ src/teachers/controller.ts
@@ -1,9 +1,18 @@
-import { JsonController, Get, Authorized } from 'routing-controllers'
+import { JsonController, Post, Body, Get, Authorized } from 'routing-controllers'
 import Teacher from './entity';
 
 @JsonController()
 export default class TeacherController {
-
+  //for test
+  @Post('/teachers')
+  async signup(
+    @Body() teacher: Teacher
+  ){
+    const {password, ...rest} = teacher
+    const entity = Teacher.create(rest)
+    await entity.setPassword(password)
+    return entity.save()
+  }
   //for test
   //@Authorized()
   @Get('/teachers')
