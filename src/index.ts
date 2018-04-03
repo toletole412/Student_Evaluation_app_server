044d6bcf8d0a4c3a36169260f74383a0b38009d4
toletole412 <yoonjioh90@gmail.com>
2018-04-03 18:39:57 +0200

    add test

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,6 @@
-import "reflect-metadata";
-import { createKoaServer } from "routing-controllers";
-import { Action, BadRequestError } from "routing-controllers";
+import app from './app'
 import setupDb from './db'
-import Controller from "./controller"
-import LoginController from './logins/controller'
-import TeacherController from './teachers/controller'
-import Teacher from './teachers/entity'
-import { verify } from "./jwt";
 
-
-const port = process.env.PORT || 4000
-
-const app = createKoaServer({
-  cors: true,
-  controllers: [
-    Controller,
-    LoginController,
-    TeacherController
-],
-
-authorizationChecker: (action: Action) => {
-    const header: string = action.request.headers.authorization
-    if (header && header.startsWith('Bearer ')) {
-       const [ , token ] = header.split(' ')
-
-       try {
-         return !!(token && verify(token))
-      }
-       catch (e) {
-         throw new BadRequestError(e)
-      }
-    }
-     return false
-   },
-
- currentUserChecker: async (action: Action) => {
-   const header: string = action.request.headers.authorization;
-   if (header && header.startsWith('Bearer ')) {
-     const [ , token ] = header.split(' ');
-
-     if (token) {
-        const { id } = verify(token)
-        return Teacher.findOneById(id)
-      }
-     }
-   }
-  })
 
 
 setupDb()
